3d7050bcd50951673899c4fc325fa3fa94d2174b
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-03-14 23:25:40 +0530

    Update WebConfiguration.java

--- src/main/java/com/example/quizapp/QuizApp/configurations/WebConfiguration.java
+++ src/main/java/com/example/quizapp/QuizApp/configurations/WebConfiguration.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","http://13.126.195.3:8080")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-//        WebMvcConfigurer.super.addCorsMappings(registry);
-    }
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:5173","http://13.126.195.3:8080")
+//                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+// //        WebMvcConfigurer.super.addCorsMappings(registry);
+//     }
 }
