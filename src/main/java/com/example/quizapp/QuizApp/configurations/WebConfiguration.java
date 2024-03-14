ddbacfd8ce37db8d8ef7542a2021ba0e435670c2
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-03-14 19:38:59 +0530

    Update WebConfiguration.java

--- src/main/java/com/example/quizapp/QuizApp/configurations/WebConfiguration.java
+++ src/main/java/com/example/quizapp/QuizApp/configurations/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
